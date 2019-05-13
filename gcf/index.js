e6ce61ebfdd6b289dbbc56575272ab22b6ca71c8
Dao Hoang Son <daohoangson@gmail.com>
2019-05-13 16:51:27 +0700

    Deploy parseXxx functions on GCF

--- gcf/index.js
+++ gcf/index.js
@@ -1,8 +1,8 @@
 "use strict";
 
-const https = require("https");
+function proxy(req, res) {
+  const https = require("https");
 
-function proxy (req, res) {
   const { url } = req.body;
   if (!url) return res.status(400).end();
 
@@ -18,12 +18,18 @@ function proxy (req, res) {
       });
 
       got.on("end", () => {
-        console.log('url=%s, status=%d, length=%d', url, got.statusCode, length);
+        console.log(
+          "url=%s, status=%d, length=%d",
+          url,
+          got.statusCode,
+          length
+        );
         res.end();
       });
     })
     .on("error", e => {
       console.error(url, e);
+      res.status(500);
 
       if (e.message) {
         res.send(e.message);
@@ -31,8 +37,36 @@ function proxy (req, res) {
         res.end();
       }
     });
-};
+}
 
 exports.proxy128 = proxy;
 exports.proxy256 = proxy;
 exports.proxy512 = proxy;
+
+function parse(req, res) {
+  const Entities = require("html-entities").AllHtmlEntities;
+  const Mercury = require("@postlight/mercury-parser");
+
+  const { url } = req.body;
+  if (!url) return res.status(400).end();
+
+  const entities = new Entities();
+
+  Mercury.parse(url).then(
+    parsed => res.send({ ...parsed, content: entities.decode(parsed.content) }),
+    e => {
+      console.error(url, e);
+      res.status(500);
+
+      if (e.message) {
+        res.send(e.message);
+      } else {
+        res.end();
+      }
+    }
+  );
+}
+
+exports.parse256 = parse;
+exports.parse512 = parse;
+exports.parse1024 = parse;
