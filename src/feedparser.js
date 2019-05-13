bdfe9731969b939b914dc57b1d7745de4a56f5ba
Dao Hoang Son <daohoangson@gmail.com>
2019-05-13 17:57:54 +0700

    Add test script for feedparser

---
+++ src/feedparser.js
@@ -0,0 +1,30 @@
+const FeedParser = require("feedparser");
+const request = require("request");
+
+const req = request("https://tuoitre.vn/rss/tin-moi-nhat.rss");
+// const req = request("https://vnexpress.net/rss/tin-moi-nhat.rss");
+const fp = new FeedParser();
+
+req.on("error", e => console.error(e));
+
+req.on("response", function(res) {
+  const resp = this;
+
+  if (res.statusCode !== 200) {
+    this.emit("error", new Error("Bad status code"));
+    return;
+  }
+
+  resp.pipe(fp);
+});
+
+fp.on("error", e => console.error(e));
+
+fp.on("readable", function() {
+  const feed = this;
+
+  var item;
+  while ((item = feed.read())) {
+    console.log(item.date, item.link);
+  }
+});
